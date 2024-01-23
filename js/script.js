3642b51f120c07d79aaacd33a4bc02faf4d8c7dd
Maksim12345676484 <maksim2000stas@gmail.com>
2024-01-23 23:12:43 +0000

    Ready header button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//animation header_navigstion_links
+
 var navigationLink = document.querySelectorAll('.navigation_link');
 
 navigationLink.forEach(function(element){
@@ -10,3 +12,15 @@ element.addEventListener('mouseleave', function() {
 })
 
 })
+//animation header_button_log_in
+
+var buttonLogIn = document.querySelector('.header_button_log_in')
+
+buttonLogIn.addEventListener('mouseenter', function(){
+    buttonLogIn.style.backgroundColor = '#f3efef'; 
+    buttonLogIn.style.color = '#0500FF'; 
+})
+buttonLogIn.addEventListener('mouseleave', function(){
+        buttonLogIn.style.backgroundColor = '#fff';
+        buttonLogIn.style.color = '#000'; 
+})   
