7279e07b886407c00ef7d4d0955fa9bda4918bba
Maksim12345676484 <maksim2000stas@gmail.com>
2024-01-24 00:08:17 +0000

    progres

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ element.addEventListener('mouseleave', function() {
 })
 //animation header_button_log_in
 
-var buttonLogIn = document.querySelector('.header_button_log_in')
+var buttonLogIn = document.querySelector('.header_button_log_in');
 
 buttonLogIn.addEventListener('mouseenter', function(){
     buttonLogIn.style.backgroundColor = '#f3efef'; 
@@ -24,3 +24,17 @@ buttonLogIn.addEventListener('mouseleave', function(){
         buttonLogIn.style.backgroundColor = '#fff';
         buttonLogIn.style.color = '#000'; 
 })   
+
+//animation header_section
+
+var pandoraSilverText1 = document.querySelector('.pandora_silver_text_1');
+
+pandoraSilverText1.addEventListener('mouseenter', function() {
+    pandoraSilverText1.style.fontSize = '68px';
+    pandoraSilverText1.style.left = '50px';
+});
+
+pandoraSilverText1.addEventListener('mouseleave', function(){
+    pandoraSilverText1.style.fontSize = ' 58.449px';
+    pandoraSilverText1.style.left = '0px';
+});
